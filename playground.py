65b720b13944fcdbf0595308866ea8d42510cda1
Anshu Sinha <anshujuly2@gmail.com>
2023-04-07 11:13:39 +0530

    Add a code on many keyword arguments

--- playground.py
+++ playground.py
@@ -7,3 +7,11 @@ def add(*args):
 
 result = add(1, 2, 3, 4, 5)
 print(result)
+
+"""*kwargs"""
+def calculate(n, **kwargs):
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(n=2, add=2, multiply=5)
