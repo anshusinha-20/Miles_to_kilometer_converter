ddb9a3915f98258c2ae877c94b35eefe9ea2d401
Anshu Sinha <anshujuly2@gmail.com>
2023-04-07 17:23:25 +0530

    Add a code on 'button, entry and component options in tkinter'

--- main.py
+++ main.py
@@ -9,7 +9,22 @@ window.minsize(width=500, height=500)
 label1 = tkinter.Label(text="I am a label", font=("Courier", 20, "bold"))
 label1.pack(expand=True)
 
+# """changing and configuring label"""
+# label1["text"] = "I'm a new label"
+# label1.config(text="I'm a new label")
 
+"""function to print on button click"""
+def buttonClicked():
+    inputText = input.get()
+    label1.config(text=inputText)
+
+"""creating button object"""
+button = tkinter.Button(text="Click me", command=buttonClicked)
+button.pack(expand=True)
+
+"""creating input object"""
+input = tkinter.Entry(width=10)
+input.pack(expand=True)
 
 """main loop to keep window open"""
 window.mainloop()
